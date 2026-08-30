e014aebcb6709d084cb7974186da0a034f92a139
Ahmed4726 <ma7985067@gmail.com>
2026-08-30 21:56:46 +0500

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -5,8 +7,21 @@ import 'core/di/service_locator.dart';
 import 'core/routes/app_router_notifier.dart';
 import 'core/services/secure_storage_service.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ⚠️ TESTING ONLY:
+  // Accepts invalid/self-signed SSL certificates.
+  HttpOverrides.global = MyHttpOverrides();
 
   await initDependencies();
 
